4f8bd1c478282aefdb1899a8398a52f10ba6d0f0
Polarts <polarbear.oc@gmail.com>
2020-01-11 00:38:22 +0200

    Added mock-routes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,29 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
+
         <Route exact path="/">
           <Root/>
         </Route>
+
+        <Route exact path="/about-author">
+          <h1>About Author</h1>
+        </Route>
+
+        <Route exact path="/about-blog">
+          <h1>About Blog</h1>
+        </Route>
+
+        <Route exact path="/post/:slug">
+          <h1>Post!</h1>
+        </Route>
+
+        <Route exact path="*">
+          <div>
+            Not found!
+          </div>
+        </Route>
+
       </Switch>
     </BrowserRouter>
   );
