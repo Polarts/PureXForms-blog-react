eb3cfa56c86ad3cb9280352bbf8708dd12e347fb
PolarBearOC <polarbear.oc@gmail.com>
2020-01-15 09:28:51 +0200

    Improved and optimized readability of WP service

--- src/services/wordpress.service.js
+++ src/services/wordpress.service.js
@@ -12,47 +12,51 @@ var tags;
 
 /**
  * Gets all the tags.
- * Tags are cached, so that if they're already fetched there's no need to fetch again.
+ * Tags are static, so they're cached to prevent extra API calls.
  * Requested fields: id, name
  * 
- * @returns an object mapped as { id: name }
+ * @returns a promise for a tags object mapped as { id: name }
  */
-export const getTagsAsync = () => new Promise( async (result) => {
-    if (!tags) {
-        let url = BASE_URL + "tags?_fields=id,name";
-        tags = {};
-        await ax.get(url).then(response => {
-            response.data.forEach(tag =>
-                tags[tag.id] = tag.name
-            );
-        });
+export const getTagsAsync = () => new Promise( 
+    async (result) => {
+        if (!tags) {
+            let url = BASE_URL + "tags?_fields=id,name";
+            tags = {};
+            await ax.get(url).then(response => {
+                response.data.forEach(tag =>
+                    tags[tag.id] = tag.name
+                );
+            });
+        }
+        result(tags);
     }
-    result(tags);
-});
+);
 
 /**
  * Gets summarized posts with query params.
- * Posts are not cached.
+ * Posts are dynamic, so they're not cached.
  * Requested fields: title, excerpt, tags, date, link
  * 
  * @param {Object} query a query params object in wordpress api format.
  * 
- * @returns an array of WP posts
+ * @returns a promise for an array of WP posts
  */
-export const getPostsAsync = (query) => new Promise( async (result) => {
-    let url = BASE_URL + "posts?";
-    
-    if (query) {
-        Object.entries(query).forEach(e => {
-            if(e[1]) {
-                url += e[0] + "=" + e[1] + "&";
-            }
-        });
+export const getPostsAsync = (query) => new Promise( 
+    async (result) => {
+        let url = BASE_URL + "posts?";
+        
+        if (query) {
+            Object.entries(query).forEach(e => {
+                if(e[1]) {
+                    url += e[0] + "=" + e[1] + "&";
+                }
+            });
+        }
+
+        url += "_fields=title,excerpt,tags,date,link";
+
+        await ax.get(url).then(response => result(response.data));
     }
-
-    url += "_fields=title,excerpt,tags,date,link";
-
-    await ax.get(url).then(response => result(response.data));
-});
+);
 
 // #endregion
