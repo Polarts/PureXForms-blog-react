d507a71ee9c865ac4f0d77d58521bef2fdfe66de
David Klein <David@Kubiyax.com>
2020-01-20 13:23:32 +0200

    Added functional posts page

--- src/routes/Post/Post.jsx
+++ src/routes/Post/Post.jsx
@@ -1,17 +1,56 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import ReactHtmlParser from 'react-html-parser';
+import { getPostsAsync } from '../../services/wordpress.service';
 
 const Post = () => {
 
     const {slug} = useParams();
 
+    // #region state
+    
+    const [postMeta, setPostMeta] = useState(null);
+    const [author, setAuthor] = useState(null);
+    const [postContent, setPostContent] = useState(null);
+    const [comments, setComments] = useState([]);
+
+    // #endregion
+
+    // #region effects
+
+    // Init effect
     useEffect(() => {
-        
-    }, []);
+        getPostsAsync({slug: slug}, ["title", "author", "date", "categories", "tags", "featured_media"])
+            .then(meta => {
+                console.log(meta);
+                setPostMeta(meta[0]);
+            });
+    }, [slug]);
+
+    // get author effect
+    useEffect(() => {
+        if(postMeta !== null) {
+
+        }
+    }, [postMeta]);
+
+    // get content effect
+    useEffect(() => {
+        if (postMeta !== null) {
+            getPostsAsync({slug: slug}, ["content"])
+                .then(response => {
+                    console.log(response);
+                    setPostContent(response[0].content.rendered);
+                });
+        }
+    }, [postMeta, slug]);
+
+    // #endregion
 
     return (
         <>
-            <h1>{slug}</h1>
+            <h1>{postMeta ? postMeta.title.rendered : slug}</h1>
+            {ReactHtmlParser(postContent)}
         </>
     )
 }
